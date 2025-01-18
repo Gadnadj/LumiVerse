30e5a9eecedebc4ba03a8440a23cb9a424a5534e
Gad Nadjar <gadnadjar@gmail.com>
2025-01-18 10:01:02 +0200

    add appstore logo

--- src/components/AppStore.tsx
+++ src/components/AppStore.tsx
@@ -1,13 +1,23 @@
-
+import AppStorePng from '../assets/app_store.png';
+import PlayStorePng from '../assets/play_store.png';
 const AppStore = () => {
     return (
-        <div>
-            <div>
-                <div className="container">
-
+        <>
+            <div className="container py-10 sm:min-h-[400px] sm:grid sm:place-items-center">
+                <div className='space-y-6 max-w-xl mx-auto'>
+                    <h1 className='text-2xl sm:text-4xl text-center font-semibold'>Get Started with our app</h1>
+                    <p className='text-center sm:px-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique magnam hardum accusantium odit?</p>
+                    <div className='flex flex-wrap items-center justify-center gap-4'>
+                        <a href="#">
+                            <img className='max-w-[150px] sm:max-w-[120px] md:max-w-[200px]' src={AppStorePng} alt="" />
+                        </a>
+                        <a href="#">
+                            <img className='max-w-[150px] sm:max-w-[120px] md:max-w-[200px]' src={PlayStorePng} alt="" />
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
