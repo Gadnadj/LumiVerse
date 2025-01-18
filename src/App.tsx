7cf34c5d1770ddae9afdd98b5f2f0ce46191d6b5
Gad Nadjar <gadnadjar@gmail.com>
2025-01-18 10:29:39 +0200

    remove player

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,10 @@ import Features from './components/Features';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
-import PopupPlayer from './components/PopupPlayer';
 import Quotes from './components/Quotes';
 
 
 function App() {
-  const [isPlay, setIsPlay] = useState<boolean>(false);
-  const tooglePlay = () => {
-    setIsPlay(true);
-  };
 
   return (
     <main className='overflow-x-hidden overflow-y-hidden bg-white dark:bg-black text-black dark:text-white duration-500'>
@@ -26,9 +21,6 @@ function App() {
       <Features />
       <AppStore />
       <Footer />
-
-      {/* Video Player */}
-      <PopupPlayer isPlay={isPlay} setIsPlay={setIsPlay} />
     </main>
   );
 }
